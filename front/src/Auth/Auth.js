f84b898b8e53fc2f17f84aa23f98eab935ee1d02
NamidaEro <horonomail@gmail.com>
2021-09-17 02:37:14 +0000

    base html pages

---
+++ front/src/Auth/Auth.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Auth(props) {
+  return <h1>Auth</h1>;
+}
+
+export default Auth;
---
+++ front/src/Auth/index.js
@@ -0,0 +1,6 @@
+import Login from './Login';
+import Auth from './Auth';
+
+
+
+export { Login, Auth };
